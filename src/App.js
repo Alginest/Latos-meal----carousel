3313d5200df9f0a495e9d1795637f3bbbda0f60d
ado durak <76266634+Alginest@users.noreply.github.com>
2022-03-26 22:04:53 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { CssBaseline } from "@material-ui/core";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer/Footer";
+import OurMenu from "./pages/OurMenu";
+import Error from "./pages/Error";
+import BlogEntries from "./pages/BlogEntries";
+function App() {
+  return (
+    <CssBaseline>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/ourMenu" element={<OurMenu />} />
+          <Route path="/blogEntries" element={<BlogEntries />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </CssBaseline>
+  );
+}
+
+export default App;
---
+++ src/pages/BlogEntries.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BlogBanner from ".././components/BloggBanner/BlogBanners";
+const BlogEntries = () => {
+  return (
+    <div>
+      <BlogBanner />
+    </div>
+  );
+};
+
+export default BlogEntries;
